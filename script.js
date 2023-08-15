7d97e2e434aad723ad518e636792064a324fef7a
Huu Nguyen Hong <huu.nguyen.hong@mediastep.com>
2023-08-15 17:36:21 +0700

    Huu

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 const burger = document.querySelector('.burger')
 const right = document.querySelector('.right')
+const userNameEle = document.getElementById('userName')
+
+const queryParams = new URLSearchParams(window.location.search)
+const name = queryParams.get('name')
+
+console.log(name)
+
+if (userNameEle) {
+  if (name) {
+    userNameEle.textContent = name
+  }
+}
 
 if (burger) {
   burger.addEventListener('click', () => {
