73724aab4e37aad4c33438f762ac71fbf3a3a36a
Huu Nguyen Hong <huu.nguyen.hong@mediastep.com>
2023-08-15 17:11:01 +0700

    Huu

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
-const nameInput = document.getElementById('nameInput');
-const content = document.getElementById('content');
-const userName = document.getElementById('userName');
+const nameInput = document.getElementById('nameInput')
+const content = document.getElementById('content')
+const userName = document.getElementById('userName')
+const burger = document.querySelector('.burger')
+const right = document.querySelector('.right')
 
-nameInput.addEventListener('keyup', function(event) {
+nameInput.addEventListener('keyup', function (event) {
   if (event.key === 'Enter') {
-    const name = nameInput.value.trim();
+    if (burger && right) {
+      right.classList.remove('active')
+    }
+
+    const name = nameInput.value.trim()
     if (name !== '') {
-      userName.textContent = name;
-      content.style.display = 'block'; // Show the content
+      userName.textContent = name
+      content.style.display = 'block' // Show the content
     } else {
-      content.style.display = 'none'; // Hide the content
+      content.style.display = 'none' // Hide the content
     }
   }
-});
+})
+
+if (burger) {
+  burger.addEventListener('click', () => {
+    if (right) {
+      right.classList.toggle('active')
+    }
+  })
+}
